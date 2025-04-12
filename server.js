2c197944c3be3b251dad1050d172adc1e18367e4
PhongTranCao <139995199+PhongTranCao@users.noreply.github.com>
2025-04-12 18:54:27 +0700

    Reapply "remove unnecessary features"
    This reverts commit ecdef2b2fe90d7257902d7111d240231c9b95d3c.

--- server.js
+++ server.js
@@ -18,32 +18,17 @@ limitations under the License.
 
 const express = require('express');
 const connectDb = require('./db/db');
-const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
 const dotenv = require('dotenv');
 dotenv.config();
-const aiRoutes = require('./routes/aiAssistentRoutes');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const announcementRoutes = require('./routes/announcementRoutes');
-const teacherRoutes = require('./routes/teachersRoutes');
-const studentRoutes = require('./routes/studentRoutes');
-const quizRoutes = require('./routes/quizessRoutes');
-const assignmentRoutes = require('./routes/assignmentRoutes');
-const connectCloudinary = require('./config/cloudinary');
 const cookieParser = require('cookie-parser');
 const socketService = require('./middlewares/socketService');
-const localAIRoutes = require('./routes/aiRoutes');
-const feedbackRouter = require('./routes/feedbackRoute');
 // Initialize Express app and setup middlewares
 const app = express();
 connectDb(); // Connect database
-connectCloudinary(); // Initialize Cloudinary
-app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174","https://virtual-classroom-admin.vercel.app","https://virtual-classroom-application.vercel.app"],
-  credentials: true
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -62,16 +47,8 @@ socketService(io);
 
 // API routes
 
-app.use('/api',aiRoutes);
 app.use('/api/v1', userRoutes);
 app.use('/api/v2', adminRoutes);
-app.use('/api/v3', announcementRoutes);
-app.use('/api/v4', teacherRoutes);
-app.use('/api/v5', studentRoutes);
-app.use('/api', quizRoutes);
-app.use('/api/v7', assignmentRoutes);
-app.use("/api/ai", localAIRoutes);
-app.use('/api',feedbackRouter);
 
 
 // Health check route
